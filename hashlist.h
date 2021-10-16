9ae5fa1ae23ed286201166cbd5f45c846a4b3a78
Timothy Rule <tim.rule.xing@gmail.com>
2021-10-16 18:22:07 +0200

    Create a Hash  List wrapper to HashMap.
    Lazy list.

---
+++ hashlist.h
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2021 Timothy Rule
+MIT License
+*/
+
+#include <stdint.h>
+#include <string.h>
+#include <hashmap.h>
+
+
+#define HASHLIST_KEY_LEN        (10+1)
+
+typedef struct HashList {
+    HashMap     hash;
+} HashList;
+
+
+static __inline__ int hashlist_init(HashList *h) {
+    return hashmap_init(&h->hash, 128, NULL);
+}
+
+static __inline__ void hashlist_destroy(HashList *h) {
+    assert(h);
+    hashmap_destroy(&h->hash);
+}
+
+static __inline__ uint32_t hashlist_length(HashList *h) {
+    assert(h);
+    return h->hash.used_nodes;
+}
+
+static __inline__ void hashlist_append(HashList *h, void *value) {
+    assert(h);
+    const char key[HASHLIST_KEY_LEN];
+    snprintf(key, HASHLIST_KEY_LEN, "%i", hashlist_length(h));
+    hashmap_set(&h->hash, key, value);
+}
+
+static __inline__ void* hashlist_get_at(HashList *h, uint32_t index) {
+    assert(h);
+    const char key[HASHLIST_KEY_LEN];
+    snprintf(key, HASHLIST_KEY_LEN, "%i", index);
+    return hashmap_get(&h->hash, key);
+}
